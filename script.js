f4685295052c238af08653041e8ee0a36943277b
Nicole Caroline <100379639+projetosnicole@users.noreply.github.com>
2024-08-10 21:18:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
   }
-  
-  
-  
+
+  window.onscroll = function() {
+    var navbar = document.getElementById("nav-bar");
+    if (window.scrollY > 50) { // Muda o valor conforme a necessidade
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+};
