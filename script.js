a73d779c76463bcae1e3eda97067dd5e6369e29e
Nicole Caroline <100379639+projetosnicole@users.noreply.github.com>
2024-08-20 17:55:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ function toggleMenu() {
     navLinks.classList.toggle('active');
   }
 
-  window.onscroll = function() {
-    var navbar = document.getElementById("nav-bar");
-    if (window.scrollY > 50) { // Muda o valor conforme a necessidade
-        navbar.classList.add("scrolled");
+document.addEventListener('scroll', function() {
+    const navLinks = document.querySelector('.nav-links');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 50) { 
+        navLinks.classList.add('nav-background');
     } else {
-        navbar.classList.remove("scrolled");
+        navLinks.classList.remove('nav-background');
     }
-};
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var acc = document.getElementsByClassName("accordion-button");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+});
